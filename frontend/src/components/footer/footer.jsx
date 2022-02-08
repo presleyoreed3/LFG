50eaf57e4584a29e507d9da0efbbbbe4fb1d764e
presleyoreed3 <presleyoreed3@gmail.com>
2022-02-07 19:00:10 -0800

    personal links

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -7,20 +7,38 @@ class Footer extends React.Component{
 		return(
 			<footer className="footer">
 				<div id="footer-content">
-					<div>
-						Info
-					</div>
 					<div id="dev-links">
-						Brian Ko
+						<h3>Info</h3>
+						<a href="">Home</a>
+						<a href="">Create Activity</a>
+						<a href=""></a>
 					</div>
-					<div id="dev-links">
-						Chris Park
-					</div>
-					<div id="dev-links">
-						Jerry Phan
-					</div>
-					<div id="dev-links">
-						Presley Reed III
+					<div id="devs">
+					The Developers
+						<div id="dev-links">
+							<h3>Brian Ko</h3>
+							<a href="https://github.com/brianko90">GitHub</a>
+							<a href="https://www.linkedin.com/in/brian-ko-ba5742229/">LinkedIn</a>
+							<a href="https://angel.co/u/brian-ko-5">AngelList</a>
+						</div>
+						<div id="dev-links">
+							<h3>Chris Park</h3>
+							<a href="https://www.github.com/cpark04">GitHub</a>
+							<a href="https://www.linkedin.com/in/chris-p-7b087b46/">LinkedIn</a>
+							<a href="https://angel.co/u/chris-park-23">AngelList</a>
+						</div>
+						<div id="dev-links">
+							<h3>Jerry Phan</h3>
+							<a href="https://github.com/jerryphan1">GitHub</a>
+							<a href="https://www.linkedin.com/in/jerry-phan-8615a7a3/">LinkedIn</a>
+							<a href="#">AngelList</a>
+						</div>
+						<div id="dev-links">
+							<h3>Presley Reed III</h3>
+							<a href="https://github.com/presleyoreed3">GitHub</a>
+							<a href="">LinkedIn</a>
+							<a href="#">AngelList</a>
+						</div>
 					</div>
 				</div>
 			</footer>
