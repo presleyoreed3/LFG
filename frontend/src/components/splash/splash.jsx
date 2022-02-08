738b886ec170c429df9d8274f5599b99395c5c91
Chris Park <cpark2653@gmail.com>
2022-02-07 17:03:33 -0800

    just renamed main_page to splash and changed corresponding imports

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-class MainPage extends React.Component {
+class SplashPage extends React.Component {
   render() {
     return (
       <div className="splash-page-background">
@@ -14,4 +14,4 @@ class MainPage extends React.Component {
   }
 }
 
-export default MainPage;
+export default SplashPage;
