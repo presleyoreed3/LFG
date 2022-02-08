9c83c144cf681b4ce6a4ea4ce7e8fa67637c1537
Chris Park <cpark2653@gmail.com>
2022-02-07 16:05:38 -0800

    added styling to background

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -3,8 +3,12 @@ import React from "react";
 class MainPage extends React.Component {
   render() {
     return (
-      <div className="splash-page-container">
-        <div className="splash-page-intro-container"></div>
+      <div className="splash-page-background">
+        <div className="splash-page-container">
+          <div className="splash-page-intro-container">
+            <h1 className="splash-page-title">LFG (LookingForGroup)</h1>
+          </div>
+        </div>
       </div>
     );
   }
