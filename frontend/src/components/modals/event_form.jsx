d84e693337b4537e92af6e8110aae52a388f68a2
BrianKo <bako2466@gmail.com>
2022-02-08 15:25:38 -0800

    finish server form

--- frontend/src/components/modals/event_form.jsx
+++ frontend/src/components/modals/event_form.jsx
@@ -18,6 +18,8 @@ class EventForm extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCategory = this.handleCategory.bind(this);
+    this.handleEventType = this.handleEventType.bind(this);
   }
 
   update(field) {
@@ -26,6 +28,7 @@ class EventForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
+    console.log(this.state);
     this.props.createEvent(this.state);
     this.setState({
       title: '',
@@ -42,13 +45,32 @@ class EventForm extends React.Component {
     })
   }
 
+  handleCategory(e) {
+    let selected = Array.from(document.getElementsByClassName('category-selector'))
+    console.log("CATEGORY", selected)
+    selected.forEach((input) => {
+      if (input.selected) this.setState({category: e.currentTarget.value})
+    })
+    console.log("state", this.state)
+  }
+
+  handleEventType(e) {
+    let selected = Array.from(document.getElementsByClassName('eventType-selector'))
+    console.log(selected)
+    selected.forEach((input) => {
+      if (input.selected) this.setState({eventType: e.currentTarget.value})
+    })
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div>
         <h2>Create an Event</h2>
+        <div onClick={this.props.closeModal}>CKLOSE MOKDAL</div>
         <form onSubmit={this.handleSubmit}>
           <label>Title
-            <input type="text" />
+            <input type="text" onChange={this.update('title')} value={this.state.title}/>
           </label>
           <label>Description
             <input type="text" onChange={this.update('description')} value={this.state.description}/>
@@ -60,26 +82,30 @@ class EventForm extends React.Component {
             <input type="datetime-local" value={this.state.eventEnd} onChange={this.update('eventEnd')}/>
           </label>
           <label>Event Type
-            <select>
-              <option value="default" disabled="true">Select an event type</option>
-              <option value="Online" onChange={this.update('eventType')}>Online</option>
-              <option value="In-Person" onChange={this.update('eventType')}>In-Person</option>
+            <select onChange={this.handleEventType} defaultValue="default">
+              <option className="eventType-selector" value="default" disabled={true}>Select an event type</option>
+              <option className="eventType-selector" value="Online">Online</option>
+              <option className="eventType-selector" value="In-Person">In-Person</option>
             </select>
           </label>
           <label>Category
-            <select>
-              <option value="default" disabled="true">Select a category</option>
-              <option value="Sport" onChange={this.update('category')}>Sport</option>
-              <option value="Gaming" onChange={this.update('category')}>Gaming</option>
-              <option value="Board-Game" onChange={this.update('category')}>Board-Game</option>
+            <select onChange={this.handleCategory} defaultValue="default">
+              <option className="category-selector" value="default" disabled={true}>Select a category</option>
+              <option className="category-selector" value="Sport">Sport</option>
+              <option className="category-selector" value="Gaming">Gaming</option>
+              <option className="category-selector" value="Board-Game">Board-Game</option>
             </select>
           </label>
           <label>Location
             <input type="text" onChange={this.update('location')} value={this.state.location}/>
           </label>
+          <label>Website
+            <input type="text" onChange={this.update('website')} value={this.state.website}/>
+          </label>
           <label>Limit
             <input type="number" onChange={this.update('limit')} value={this.state.limit}/>
           </label>
+          <button>Create Event</button>
         </form>
       </div>
     )
