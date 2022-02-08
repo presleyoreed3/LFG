8143d91cc609ce8440a79779e36a0a3389cfd188
BrianKo <bako2466@gmail.com>
2022-02-08 13:59:38 -0800

    work on serverform modal

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -8,7 +8,7 @@ import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 import FooterContainer from './footer/footer_container'
 import HomeContainer from "./home/home_container";
-import Modal from './modal/modal'
+import Modal from './modals/modal'
 import EventShowContainer from './event/events_show_container';
 import '../app.scss'
 
--- frontend/src/components/modals/event_form_container.js
+++ frontend/src/components/modals/event_form_container.js
@@ -1,18 +1,19 @@
 import {connect} from 'react-redux';
 import EventForm from './event_form';
-import {openModal, closeModal} from '../../actions/modal_actions';
-
+// import {openModal, closeModal} from '../../actions/modal_actions';
+import { createEvent } from '../../actions/event_actions';
 
 const mSTP = state => {
   return {
-    
+    user: state.session.user
   }
 }
 
 const mDTP = dispatch => {
   return {
-    closeModal: () => dispatch(closeModal()),
-    openModal: (modal) => dispatch(openModal(modal))
+    // closeModal: () => dispatch(closeModal()),
+    // openModal: (modal) => dispatch(openModal(modal)),
+    createEvent: (event) => dispatch(createEvent(event))
   }
 }
 
