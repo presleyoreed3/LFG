9361363142d494e2af605506696653eceff44944
presleyoreed3 <presleyoreed3@gmail.com>
2022-02-09 12:02:10 -0800

    button outline correction

--- frontend/src/components/event/events_show_container.js
+++ frontend/src/components/event/events_show_container.js
@@ -7,14 +7,12 @@ const mSTP = (state,ownProps) => {
   return {
     events: state.entities.events,
     loggedIn: state.session.isAuthenticated,
-    // event: state.entities.events[ownProps.match.params.eventId]
   }
 }
 
 const mDTP = (dispatch) => {
   return {
     fetchEvents: () => dispatch(fetchEvents()),
-    // fetchEvent: (eventId) => dispatch(fetchEvent(eventId)),
     openModal: (modal, eventId) => dispatch(openModal(modal, eventId))
   }
 }
