db79e855425996b074848b668ecac185485d72bd
Chris Park <cpark2653@gmail.com>
2022-02-09 09:38:34 -0800

    added styling changes to footer/nav/event_form

--- frontend/src/components/modals/event_form.jsx
+++ frontend/src/components/modals/event_form.jsx
@@ -69,46 +69,106 @@ class EventForm extends React.Component {
         <h2>Create an Event</h2>
         <div onClick={this.props.closeModal}>CLOSE MODAL</div>
         <form onSubmit={this.handleSubmit}>
-          <label>Title
-            <input type="text" onChange={this.update('title')} value={this.state.title}/>
+          <label>
+            Title
+            <input
+              type="text"
+              onChange={this.update("title")}
+              value={this.state.title}
+            />
           </label>
-          <label>Description
-            <input type="text" onChange={this.update('description')} value={this.state.description}/>
+          <label>
+            Description
+            <textarea
+              type="text"
+              onChange={this.update("description")}
+            >
+              {this.state.description}
+            </textarea>
           </label>
-          <label>Event Start
-            <input type="datetime-local" value={this.state.eventStart} onChange={this.update('eventStart')}/>
+          <label>
+            Event Start
+            <input
+              type="datetime-local"
+              value={this.state.eventStart}
+              onChange={this.update("eventStart")}
+            />
           </label>
-          <label>Event End
-            <input type="datetime-local" value={this.state.eventEnd} onChange={this.update('eventEnd')}/>
+          <label>
+            Event End
+            <input
+              type="datetime-local"
+              value={this.state.eventEnd}
+              onChange={this.update("eventEnd")}
+            />
           </label>
-          <label>Event Type
+          <label>
+            Event Type
             <select onChange={this.handleEventType} defaultValue="default">
-              <option className="eventType-selector" value="default" disabled={true}>Select an event type</option>
-              <option className="eventType-selector" value="Online">Online</option>
-              <option className="eventType-selector" value="In-Person">In-Person</option>
+              <option
+                className="eventType-selector"
+                value="default"
+                disabled={true}
+              >
+                Select an event type
+              </option>
+              <option className="eventType-selector" value="Online">
+                Online
+              </option>
+              <option className="eventType-selector" value="In-Person">
+                In-Person
+              </option>
             </select>
           </label>
-          <label>Category
+          <label>
+            Category
             <select onChange={this.handleCategory} defaultValue="default">
-              <option className="category-selector" value="default" disabled={true}>Select a category</option>
-              <option className="category-selector" value="Sport">Sport</option>
-              <option className="category-selector" value="Gaming">Gaming</option>
-              <option className="category-selector" value="Board-Game">Board-Game</option>
+              <option
+                className="category-selector"
+                value="default"
+                disabled={true}
+              >
+                Select a category
+              </option>
+              <option className="category-selector" value="Sport">
+                Sport
+              </option>
+              <option className="category-selector" value="Gaming">
+                Gaming
+              </option>
+              <option className="category-selector" value="Board-Game">
+                Board-Game
+              </option>
             </select>
           </label>
-          <label>Location
-            <input type="text" onChange={this.update('location')} value={this.state.location}/>
+          <label>
+            Location
+            <input
+              type="text"
+              onChange={this.update("location")}
+              value={this.state.location}
+            />
           </label>
-          <label>Website
-            <input type="text" onChange={this.update('website')} value={this.state.website}/>
+          <label>
+            Website
+            <input
+              type="text"
+              onChange={this.update("website")}
+              value={this.state.website}
+            />
           </label>
-          <label>Limit
-            <input type="number" onChange={this.update('limit')} value={this.state.limit}/>
+          <label>
+            Limit
+            <input
+              type="number"
+              onChange={this.update("limit")}
+              value={this.state.limit}
+            />
           </label>
           <button>Create Event</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
