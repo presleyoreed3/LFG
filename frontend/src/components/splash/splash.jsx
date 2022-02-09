921d299eacf17276d8ed002dc22313d8821234ca
presleyoreed3 <presleyoreed3@gmail.com>
2022-02-09 15:34:34 -0800

    fixed clear navbar layout

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -7,6 +7,7 @@ class SplashPage extends React.Component {
       <div className="splash-page-background">
         <div className="splash-page-container">
           <div className="splash-page-intro-container">
+            <img src="https://some-trails-aa-dev.s3.us-west-1.amazonaws.com/lfg-logo-green.png" />
             <h1 className="splash-page-title">LFG</h1>
           </div>
         </div>
