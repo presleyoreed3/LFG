8d7b3ac3714a8895917c018dc778f6df04661784
Jerry Phan <jerryphan1@gmail.com>
2022-02-09 17:43:39 -0800

    delete button style

--- frontend/src/components/comments/comment_delete.jsx
+++ frontend/src/components/comments/comment_delete.jsx
@@ -17,13 +17,9 @@ export default class CommentDelete extends React.Component {
   render(){
     return (
       <div id='delete-modal'>
-        <h1 className="delete-modal-header">Delete comment</h1>
-        <p className="delete-modal-text">Delete your comment permanently?</p>
-        <div id='delete-modal-button-container'>
+        <p className="delete-modal-text">Are you sure you want to delete this comment?</p>
           <button className="delete-button-cancel" onClick={() => this.props.closeModal()}>CANCEL</button>
           <button className="delete-button-confirm" onClick={this.handleClick}>DELETE</button>
-        </div>
-
       </div>
     )
   }
