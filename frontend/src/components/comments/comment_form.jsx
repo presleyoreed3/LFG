7a72a45666cbc04cd6786ca107c271ade02b7d15
presleyoreed3 <presleyoreed3@gmail.com>
2022-02-09 17:21:10 -0800

    scrolling on comments

--- frontend/src/components/comments/comment_form.jsx
+++ frontend/src/components/comments/comment_form.jsx
@@ -71,7 +71,8 @@ class CommentForm extends React.Component{
             <textarea onKeyUp={this.handleErrors} className="comment-form-textarea" placeholder='Add a public comment...' value={this.state.text} onChange={this.update('text')}/>
             <div className="comment-form-buttons">
               <button className='comment-form-input' type='submit' placeholder="Comment" disabled={status}>Submit</button>
-              <button className='comment-form-button' onClick={this.handleCancel} disabled={status}>Cancel</button>            </div>
+              <button className='comment-form-button' onClick={this.handleCancel} disabled={status}>Cancel</button> 
+            </div>
           </form>
         </div>
       </div>
--- frontend/src/components/comments/comment_index.jsx
+++ frontend/src/components/comments/comment_index.jsx
@@ -38,16 +38,17 @@ class CommentIndex extends React.Component {
       <div className="comment-index-container">
           <h2>{EventsComments.length} Comments</h2>
             <CommentForm eventId={this.props.eventId}  user={this.props.currentUser} createComment={this.props.createComment} />
-          {
-            EventsComments.map((comment) => {
+          <div id="comments-holder">
+          {EventsComments.map((comment) => {
               return <CommentIndexItem key={comment._id} index={comment._id} comment={comment} users={this.props.users} 
               currentUser={this.props.currentUser} openModal={this.props.openModal}/>
             })
           }
+          </div>
       </div>
     )
   }
-  
+
 }
 
 export default CommentIndex;
