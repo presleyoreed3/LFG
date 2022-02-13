e239fee1b3dedcb5a64a8bd7720e722e966df053
presleyoreed3 <presleyoreed3@gmail.com>
2022-02-12 17:28:45 -0800

    full comment errors

--- frontend/src/components/comments/comment_edit.jsx
+++ frontend/src/components/comments/comment_edit.jsx
@@ -45,7 +45,7 @@ class CommentEdit extends React.Component {
     return (
       <ul>
         {Object.keys(this.state.errors).map((error, i) => (
-          <li key={`error-${i}`}>{this.state.errors[error]}</li>
+          <li id="comment-errors" key={`error-${i}`}>{this.state.errors[error]}</li>
         ))}
       </ul>
     );
