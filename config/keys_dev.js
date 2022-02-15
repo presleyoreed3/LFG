dfc51907ca786aa0e6f2ae324fe4ffaf2a888162
Chris Park <cpark2653@gmail.com>
2022-02-14 17:27:12 -0800

    changed database

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://merngroup:7ZEVwDR1jvLuzxxV@cluster0.96phx.mongodb.net/LFGDatabase",
+    "mongodb+srv://merngroup:7ZEVwDR1jvLuzxxV@cluster0.96phx.mongodb.net/LFGDatabase2",
   secretOrKey: "7ZEVwDR1jvLuzxxV",
 };
