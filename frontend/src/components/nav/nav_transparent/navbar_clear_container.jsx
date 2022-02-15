ed3c89a9b001a2d8baf30215d5fb9e28f115d1f3
presleyoreed3 <presleyoreed3@gmail.com>
2022-02-14 16:28:44 -0800

    create working on home

--- frontend/src/components/nav/nav_transparent/navbar_clear_container.jsx
+++ frontend/src/components/nav/nav_transparent/navbar_clear_container.jsx
@@ -1,10 +1,16 @@
 import { connect } from "react-redux";
 import { logout } from "../../../actions/session_actions";
+import {openModal} from '../../../actions/modal_actions';
 
 import NavBarClear from "./navbar_clear";
 
 const mapStateToProps = (state) => ({
-  loggedIn: state.session.isAuthenticated,
+  loggedIn: state.session.isAuthenticated,  
 });
 
-export default connect(mapStateToProps, { logout })(NavBarClear);
+const mapDispatchToProps = dispatch => ({
+  openModal: (modal, eventId) => dispatch(openModal(modal, eventId)),
+  logout: () => dispatch(logout())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBarClear);
